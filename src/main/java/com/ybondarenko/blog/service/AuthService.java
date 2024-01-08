0f93d48fd61589d79d56a9d0b547a64772eed6de
Yuriy Bondarenko <molecula1356@gmail.com>
2024-01-08 16:14:42 +0300

    add password encoder

--- src/main/java/com/ybondarenko/blog/service/AuthService.java
+++ src/main/java/com/ybondarenko/blog/service/AuthService.java
@@ -4,6 +4,7 @@ import com.ybondarenko.blog.dto.RegisterRequest;
 import com.ybondarenko.blog.entity.User;
 import com.ybondarenko.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,17 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
