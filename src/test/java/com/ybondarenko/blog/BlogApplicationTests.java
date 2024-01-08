c2e66630f0821ee319bcb79d60525636e1c5460d
Yuriy Bondarenko <molecula1356@gmail.com>
2024-01-08 13:53:47 +0300

    init

---
+++ src/test/java/com/ybondarenko/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybondarenko.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
